da9ff29fe27a78bdb2a7976e0e5ad6269b669a47
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2020-06-07 13:03:50 -0500

    is that right

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_apigatewayv2_api" "api" {
 resource "aws_apigatewayv2_integration" "collie" { 
   api_id = aws_apigatewayv2_api.api.id
   integration_type = "AWS_PROXY"
-  integration_method = "ANY"
+  integration_method = "POST"
   integration_uri = aws_lambda_function.collie.invoke_arn
   payload_format_version = "2.0"
 }
