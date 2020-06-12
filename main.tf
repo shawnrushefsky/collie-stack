d4023e5edede4650a629b4466ce7ef4babe1e9ec
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2020-06-12 15:03:14 -0500

    smaller batch size

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ resource "aws_sqs_queue" "indexing_queue" {
 }
 
 resource "aws_lambda_event_source_mapping" "event_source_mapping" {
-  batch_size       = 100
+  batch_size       = 10
   event_source_arn = aws_sqs_queue.indexing_queue.arn
   enabled          = true
   function_name    = aws_lambda_function.collie_indexer.arn
